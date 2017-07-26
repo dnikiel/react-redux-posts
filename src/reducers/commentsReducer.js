fa24fe76dba400bbfd4f946c5e62558ede4893b5
Dominik Nikiel <dominiknikiel@gmail.com>
2017-07-26 14:44:56 +0200

    flow added

--- src/reducers/commentsReducer.js
+++ src/reducers/commentsReducer.js
@@ -1,10 +1,18 @@
+// @flow
+
+type State = {
+  comments: Object,
+  isLoading: boolean,
+  fetchError: ?Object
+}
+
 const commentsInitialState = {
   comments: {},
   isLoading: false,
   fetchError: null
 }
 
-const commentsReducer = (state = commentsInitialState, action) => {
+const commentsReducer = (state: State = commentsInitialState, action: Object): State => {
   switch (action.type) {
     case 'FETCH_COMMENTS_STARTED':
       return {
